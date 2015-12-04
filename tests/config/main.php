95cbf76ec931dedc6d798d9680678482052e1e6a
maksim.d <maksim.d@mail.int>
2015-12-04 22:24:14 +0200

    Delete codeception; Add phpunit

---
+++ tests/config/main.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maksim.d
+ * Date: 12/4/15
+ * Time: 22:17
+ */
+return [];
---
+++ tests/game/outside/GameTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maksim.d
+ * Date: 12/4/15
+ * Time: 22:21
+ */
+
+namespace tests\game\outside;
+
+
+class GameTest extends \PHPUnit_Framework_TestCase
+{
+
+}
